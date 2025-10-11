8fe5d3e6afda7b20559ca14ad7fff4272cd3804b
Laura <laurayoung305@gmail.com>
2025-10-11 18:13:49 +1100

    sticky sticky fingers

--- wisdomplaybook.py
+++ wisdomplaybook.py
@@ -1,13 +1,13 @@
 import gspread as gs
 import plotly as pl
-import json
+import toml
 from google.oauth2.service_account import Credentials
 import streamlit as st
 import pandas as pd
 from urllib.parse import urlparse, parse_qs
 
 #Google sheet setup
-creds_info = json.loads(st.secrets["GOOGLE_CREDS"]["GOOGLE_CREDS_JSON"])
+creds_info = toml.loads(st.secrets["GOOGLE_CREDS"])
 
 print(creds_info)
 
