032d217e93c3dc0a1e5f8c061892fa81d0e25cbe
Laura <laurayoung305@gmail.com>
2025-10-25 23:46:48 +1100

    going back

--- wisdomplaybook_old.py
+++ wisdomplaybook_old.py
@@ -362,21 +362,18 @@ def trait_plots(uuid, data, TRAIT_COLS, TRAIT_RANGES, peer_data=None):
         question_scores = pd.to_numeric(user_row[question_cols], errors='coerce').fillna(0).tolist()
 
         # --- Get peer scores if provided ---
-
-        st.write("PEER")
-        st.write(peer_data)
-        st.write("USER")
-        st.write(data)
         st.write(raw_range)
         st.write('show')
+        st.write(peer_data)
         st.write(question_cols)
         st.write(len([i for i in raw_range]))
         st.write(peer_data.iloc[:, counter : counter + len([i for i in raw_range])])
-        # if peer_data is not None:
-        #     #peer_scores = pd.to_numeric(peer_data[question_cols].mean(), errors='coerce').fillna(0).tolist()
-        #     peer_scores = pd.to_numeric(peer_data.iloc[:, counter:len([i for i in raw_range])].mean(), errors='coerce')#.fillna(0).tolist()
-        # else:
-        #     peer_scores = [0] * len(question_cols)
+        #question_cols = data.iloc[:, counter:len([i for i in raw_range])]
+        if peer_data is not None:
+            #peer_scores = pd.to_numeric(peer_data[question_cols].mean(), errors='coerce').fillna(0).tolist()
+            peer_scores = pd.to_numeric(peer_data.iloc[:, counter:len([i for i in raw_range])].mean(), errors='coerce')#.fillna(0).tolist()
+        else:
+            peer_scores = [0] * len(question_cols)
         
         # --- Create grouped horizontal bar chart ---
         bar_fig = go.Figure()
