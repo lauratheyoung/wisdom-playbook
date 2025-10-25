f29cbe209ff94b98c53d8ad77befd50092d0114e
Laura <laurayoung305@gmail.com>
2025-10-25 17:27:03 +1100

    editting graph 1

--- wisdomplaybook.py
+++ wisdomplaybook.py
@@ -322,7 +322,7 @@ def trait_plots(uuid, data, TRAIT_COLS, TRAIT_RANGES):
         ))
         bar_fig.update_layout(
             title=dict(
-                text=f"{trait} - Individual Question Scores",
+                title_text=f"{trait} - Individual Question Scores",
                 title_font=dict(family='Inter, sans-serif', size=20, color='black')
             ),
             yaxis=dict(
