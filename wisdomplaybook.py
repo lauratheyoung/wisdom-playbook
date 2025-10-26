8732f90bfcb5876bc41b06d72518585aa2e219e5
Laura <laurayoung305@gmail.com>
2025-10-26 15:56:11 +1100

    going back

--- wisdomplaybook.py
+++ wisdomplaybook.py
@@ -539,6 +539,9 @@ st.plotly_chart(fig, use_container_width=True)
 df_user_data = prepare_peer_data(data)
 df_peer_data = prepare_peer_data(peerdata)
 
+st.write("FORMAT PEER")
+st.write(df_peer_data)
+
 user_row = get_user_row_by_uuid(df_user_data, uuid_input)
 user_peer_data = get_peer_data_from_user_row(df_peer_data, user_row)
 
