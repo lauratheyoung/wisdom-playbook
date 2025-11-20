d8cafbc68f901619a39da05d0c5a712a3c434a7c
Laura <laurayoung305@gmail.com>
2025-11-20 14:49:17 +1100

    testing new UI

--- wisdomplaybook.py
+++ wisdomplaybook.py
@@ -725,7 +725,7 @@ def trait_plots(uuid, user_row, TRAIT_COLS, TRAIT_RANGES, user_peer_data):
         #         </div>
         #     """, unsafe_allow_html=True)
 
-        with st.expander(f"{trait} — Click to see definition", expanded=False, icon=None):
+        with st.expander(f"{trait} — Click to see definition", expanded=False, icon="🌱"):
             st.markdown(f"""
                 <div style='
                     background-color:#F7F7F7;
